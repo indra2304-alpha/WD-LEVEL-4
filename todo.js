7640a302b6cdf5d5ef46209c27d1dd61bf5767f0
JAI INDRA REDDY <81897404+indra2304-alpha@users.noreply.github.com>
2022-11-19 01:13:58 -0800

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,30 +1,59 @@
-const todoList = () => {
-  let all = [];
-  const add = (todoItem) => {
-    all.push(todoItem);
-  };
-  const markAsComplete = (index) => {
-    all[index].completed = true;
-  };
+/* eslint-disable no-undef */
+const todoList = require("../todo");
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
-  const overdue = () => {
-    return all.filter(
-      (item) => item.dueDate < new Date().toLocaleDateString("en-CA")
-    );
-  };
+describe("Todo List Test Suite", () => {
+  beforeAll(() => {
+    const today = new Date();
+    const oneDay = 86400000;
+    [
+      {
+        title: "Prepare for Exam",
+        completed: false,
+        dueDate: new Date(today.getTime() - oneDay).toLocaleDateString(
+          "en-CA"
+        ),
+      },
+      {
+        title: "Pay rent",
+        completed: false,
+        dueDate: new Date().toLocaleDateString("en-CA"),
+      },
+      {
+        title: "Submit assignment",
+        completed: false,
+        dueDate: new Date(today.getTime() + oneDay).toLocaleDateString(
+          "en-CA"
+        ),
+      },
+    ].forEach(add);
+  });
+  test("checks creating a new todo", () => {
+    expect(all.length).toEqual(3);
+    add({
+      title: "Go Buy Bred",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
 
-  const dueToday = () => {
-    return all.filter(
-      (item) => item.dueDate === new Date().toLocaleDateString("en-CA")
-    );
-  };
+    expect(all.length).toEqual(4);
+  });
 
-  const dueLater = () => {
-    return all.filter(
-      (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
-    );
-  };
-  return { all, add, markAsComplete, overdue, dueToday, dueLater };
-};
+  test("checks marking a todo as completed", () => {
+    expect(all[0].completed).toEqual(false);
+    markAsComplete(0);
+    expect(all[0].completed).toEqual(true);
+  });
 
-module.exports = todoList;
+  test("checks retrieval of overdue items", () => {
+    expect(overdue().length).toEqual(1);
+  });
+
+  test("checks retrieval of due today items", () => {
+    expect(dueToday().length).toEqual(2);
+  });
+
+  test("checks retrieval of due later items", () => {
+    expect(dueLater().length).toEqual(1);
+  });
+});
